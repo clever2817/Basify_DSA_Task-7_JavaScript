c1f92e96c8d84446a4499db7f5c751ad49daab22
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-30 15:25:19 +0600

    Problem - 4
    Problem - 4

--- day2.js
+++ day2.js
@@ -30,3 +30,22 @@ if (age < 13) {
 } else {
   console.log("senior");
 }
+
+//Problem-4 (Grade Distribution)
+let score = 80;
+
+if (score >= 80 && score <= 100) {
+  console.log("A+");
+} else if (score >= 70 && score < 80) {
+  console.log("A");
+} else if (score >= 60 && score < 70) {
+  console.log("A-");
+} else if (score >= 50 && score < 60) {
+  console.log("B");
+} else if (score >= 40 && score < 50) {
+  console.log("C");
+} else if (score >= 33 && score < 40) {
+  console.log("D");
+} else {
+  console.log("F");
+}
