00c8d1d1864df5adb9025f4ce8da66db72928950
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-30 15:03:46 +0600

    Problem - 1
    Problem - 1

---
+++ day2.js
@@ -0,0 +1,10 @@
+// Problem-1 (Positive/Negative/Zero Number)
+let number = 10;
+
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
