fcde38cf6d47cb9c7b3baabd39d69b9aae11349b
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-30 18:12:54 +0600

    Problem - 10
    Problem - 10

--- day2.js
+++ day2.js
@@ -113,3 +113,20 @@ if (number4 >= 1 && number4 <= 10) {
 } else {
   console.log(false);
 }
+
+// Problem-10 (Largest among 3 numbers)
+
+let num1 = 10;
+let num2 = 25;
+let num3 = 15;
+let largest = 0;
+
+if (num1 > num2 && num1 > num3) {
+  largest = num1;
+} else if (num2 > num1 && num2 > num3) {
+  largest = num2;
+} else {
+  largest = num3;
+}
+
+console.log(largest);
