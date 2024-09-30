2b60b1c4e3f1cad5ecb8ed4629407331c09a391d
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-30 15:06:57 +0600

    Problem - 2
    Problem - 2

--- day2.js
+++ day2.js
@@ -1,10 +1,19 @@
 // Problem-1 (Positive/Negative/Zero Number)
-let number = 10;
+let number1 = 10;
 
-if (number > 0) {
+if (number1 > 0) {
   console.log("Positive");
-} else if (number < 0) {
+} else if (number1 < 0) {
   console.log("Negative");
 } else {
   console.log("Zero");
+}
+
+//Problem-2 (Even/Odd Problem)
+let number2 = 100;
+
+if (number2 % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
 }
