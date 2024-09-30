c077cbbc46fa2e3673dcaa28313ceb4a17a5fe45
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-09-30 15:12:22 +0600

    Problem - 3
    Problem - 3

--- day2.js
+++ day2.js
@@ -17,3 +17,16 @@ if (number2 % 2 === 0) {
 } else {
   console.log("odd");
 }
+
+// Problem-3 (Classify according to age)
+let age = 40;
+
+if (age < 13) {
+  console.log("child");
+} else if (age >= 13 && age < 20) {
+  console.log("teenager");
+} else if (age >= 20 && age < 65) {
+  console.log("adult");
+} else {
+  console.log("senior");
+}
